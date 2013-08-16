37bd55d5888de2bd22938409844b5d638943272a
Abhishek Bajpayee <ab9@Abhisheks-Studio-XPS.(none)>
2013-08-16 13:41:16 -0400

    updated email script

--- python/scripts/libmail.py
+++ python/scripts/libmail.py
@@ -1,19 +1,17 @@
 import smtplib
+from email.mime.text import MIMEText
 
 def send_email(to, subject, body):
 
-    msg = "\r\n".join([
-            "From: ehlatmit@gmail.com",
-            "To: ", to,
-            "Subject: ", subject,
-            "",
-            body
-            ])
+    msg = MIMEText(body)
+    msg['From'] = 'ehlatmit@gmail.com'
+    msg['To'] = to
+    msg['Subject'] = subject
 
     server = smtplib.SMTP('smtp.gmail.com',587) #port 465 or 587
     server.ehlo()
     server.starttls()
     server.ehlo()
     server.login('ehlatmit@gmail.com','ehlwave2013')
-    server.sendmail('ehlatmit@gmail.com', to, body)
+    server.sendmail('ehlatmit@gmail.com', to, msg.as_string())
     server.close()
