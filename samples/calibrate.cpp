b65dad1592d65ea3e6b0061563ee13676269722d
Abhishek <ab9@EHL-PE--T620>
2016-02-11 19:08:52 -0500

    completed calibration tutorial

--- samples/calibrate.cpp
+++ samples/calibrate.cpp
@@ -1,9 +1,9 @@
 #include "std_include.h"
 
 #include "calibration.h"
-#include "refocusing.h"
-#include "pLoc.h"
-#include "tracking.h"
+// #include "refocusing.h"
+// #include "pLoc.h"
+// #include "tracking.h"
 #include "tools.h"
 
 using namespace cv;
@@ -24,13 +24,11 @@ int main(int argc, char** argv) {
     google::ParseCommandLineFlags(&argc, &argv, true);
     init_logging(argc, argv);
 
-    Size grid_size = Size(FLAGS_hgrid, FLAGS_vgrid); // Format (horizontal_corners, vertical_corners)
-
     // Uses dummy mode
-    multiCamCalibration calibration(FLAGS_path, grid_size, FLAGS_gridsize, FLAGS_ref, 1, FLAGS_mtiff, FLAGS_skip, FLAGS_show_corners);
+    multiCamCalibration calibration(FLAGS_path, Size(FLAGS_hgrid, FLAGS_vgrid), FLAGS_gridsize, FLAGS_ref, 1, FLAGS_mtiff, FLAGS_skip, FLAGS_show_corners);
     calibration.run();
 
-    cout<<"DONE!"<<endl;
+    LOG(INFO)<<"DONE!";
     
     return 1;
 
