510e40df5bd55a590e18d805e0650285738b217a
Abhishek <ab9@EHL-PE--T620>
2017-03-18 18:52:34 -0400

    removing src and include

--- include/parse_safe_settings.h
+++
@@ -1,9 +0,0 @@
-#include "std_include.h"
-
-#include "safeRefocusing.h"
-#include "typedefs.h"
-
-using namespace cv;
-using namespace std;
-
-void parse_safe_settings(string filename, safe_refocus_settings &safe_settings, bool help);
