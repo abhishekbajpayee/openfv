4343190dc5790f17d307311430c3d81f011671d2
Elise Xue <eyxue@mit.edu>
2017-04-14 13:55:17 -0400

    updated docstring script to automatically find files and format docstrings better

--- docs/openfv_python_doc_build.py
+++ docs/openfv_python_doc_build.py
@@ -3,15 +3,24 @@ import sys
 from tempfile import mkstemp
 from shutil import move
 from os import remove, close
+import glob
+
+h = set(glob.glob('../include/*.h'))
+cpp = set(glob.glob('../src/modules/*.cpp'))
+hfiles = set([lambda x: x.replace("../include/", "").replace(".h", "") for filee in h])
+cppfiles = set([lambda x: x.replace("../src/modules/", "").replace(".cpp", "") for filee in cpp])
+shared = h.intersection(cpp)
 
 # returns a list of strings
 # enters under the "DocString"
 def parse_doc_string(istr):
-    param_pattern = re.compile(r'\\(\w+)\s+(.*)')
+    param_pattern = re.compile(r'\\param\s+(.*)')
+    return_pattern = re.compile(r'\\return\s+(.*)')
     line_comment_pattern = re.compile(r'//!\s*(\w+)\s+(.*)')
     block_comment_pattern = re.compile(r'/\*!\s*(\w+)\s+(.*)')
     docstring = list()
     # for line in map(lambda s : s.strip(), istr):
+    has_param = False
     for line in istr:
         line = line.strip()
         if line_comment_pattern.match(line):
@@ -19,14 +28,31 @@ def parse_doc_string(istr):
             doc = doc.lstrip('//! ')
             docstring.append(doc)
             return docstring
-        if block_comment_pattern.match(line):
+        elif block_comment_pattern.match(line):
             doc = block_comment_pattern.match(line).group()
             doc = doc.lstrip('/*! ')
             docstring.append(doc)
-        if param_pattern.match(line):
+        elif param_pattern.match(line):
             match = param_pattern.match(line)
-            docstring.append(match.group(1) + ": " + match.group(2))
-        if line == '*/':
+            words = match.group(1).split(" ")
+            param_name = words[0]
+            description = " ".join(words[1:])
+            if not has_param:
+                docstring.append("Parameters")
+                docstring.append("----------")
+                has_param = True
+            docstring.append(param_name)
+            docstring.append("    " + description)
+        elif return_pattern.match(line):
+            match = return_pattern.match(line)
+            words = match.group(1).split(" ")
+            return_type = words[0]
+            description = " ".join(words[1:])
+            docstring.append("Return")
+            docstring.append("-------")
+            docstring.append(return_type)
+            docstring.append("    " + description)
+        elif line == '*/':
             return docstring
 
 def extract(istr, docstrings):
@@ -62,11 +88,12 @@ def substitute(istr, docfile, docstrings):
     move(abs_path, docfile)
 
 if __name__ == '__main__':
-    sourcefile = sys.argv[1]
-    docfile = sys.argv[2]
-    docstrings = dict()
-    with open(sourcefile) as istr:
-        extract(istr, docstrings)
-    with open(docfile) as istr:
-        # with sys.stdout as ostr:
-        substitute(istr, docfile, docstrings)
+    for filename in shared:
+        sourcefile = '../include/' + filename + '.h'
+        docfile = '../src/modules/' + filename + '.cpp'
+        docstrings = dict()
+        with open(sourcefile) as istr:
+            extract(istr, docstrings)
+        with open(docfile) as istr:
+            # with sys.stdout as ostr:
+            substitute(istr, docfile, docstrings)
