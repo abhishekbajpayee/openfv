a4620dcef55c129a44c0dff77f439efe3a4627fa
unknown <bpaez@g.hmc.edu>
2020-04-18 17:19:15 -0700

    Updated some opencv paths

--- include/openfv/std_include.h
+++ include/openfv/std_include.h
@@ -27,7 +27,7 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <opencv2/features2d/features2d.hpp>
-#include <opencv2/nonfree/features2d.hpp>
+//#include <opencv2/nonfree/features2d.hpp>
 
 // Standard headers
 #include <iostream>
