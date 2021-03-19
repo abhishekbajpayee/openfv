f39fa609effc8ae215e1192d430337e91bed52fe
fifteencent <tiandingdong@gmail.com>
2021-03-19 14:19:35 -0700

    Added verbosity logs to refCalib, completed the logger python file.
    Co-authored-by: Albany Blackburn <a-blackburn@users.noreply.github.com>

--- test/test.py
+++ test/test.py
@@ -1,14 +1,6 @@
 import glog as log
-from absl import app
-from absl import flags
+import argparse
 # FLAGS = flags.FLAGS
-MAX = 4
-
-def VLOG(vLevel,string):
-    log.log(MAX-vLevel+1,string)
-def setLevel(level):
-    log.logger.setLevel(MAX-level+1)
-    log.logger.debug('Log level set to %s', level)
 
 log.info("It works.")
 log.warn("Something not ideal")
@@ -22,24 +14,22 @@ log.fatal("AAAAAAAAAAAAAAA!")
 #   if FLAGS.age is not None:
 #     print('You are %d years old, and your job is %s' % (FLAGS.age, FLAGS.job))
 
-FLAGS = flags.FLAGS
-
 # flags.DEFINE_boolean('debug', False, 'Produces debugging output.')
 
-def main(argv):
-    # if FLAGS.debug:
-    #     print('non-flag arguments:', argv)
-    print('The value of myflag is %s' % FLAGS.verbosity)
-    setLevel(FLAGS.verbosity)
-    VLOG(4, "four, most descriptive")
-    VLOG(3, "three")
-    VLOG(2, "two")
-    VLOG(1, "one, least desciptive")
 #   log.log(4, "four, least descriptive")
 #   log.log(3, "three")
 #   log.log(2, "two")
-#   log.log(1, "one, most desciptive")
+#   log.log(1, "one, most descriptive")
 
 
 if __name__ == '__main__':
-  app.run(main)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbosity', help='verbosity level for file prints', type=int)
+    args = parser.parse_args()
+
+    print('The value of myflag is %s' % args.verbosity)
+    log.setLevel(args.verbosity)
+    log.VLOG(4, "four, most descriptive")
+    log.VLOG(3, "three")
+    log.VLOG(2, "two")
+    log.VLOG(1, "one, least descriptive")
